59010220d4bc8efd9af814844166134061808857
Paritosh7 <paritosh7.sahni@gmail.com>
2021-10-12 16:54:11 +0530

    styled-components-setup

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import GlobalStyles from "./GlobalStyles/GobalStyles";
+
+function App() {
+  return (
+    <div className="App">
+      Hello
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
